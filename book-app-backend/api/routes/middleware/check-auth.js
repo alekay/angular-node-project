7270df3010d47761e76d874ce3d820cde2b439ba
alekay <alekaymusic@gmail.com>
2021-06-16 17:52:04 -0400

    feat: add middleware for authorization

---
+++ book-app-backend/api/routes/middleware/check-auth.js
@@ -0,0 +1,17 @@
+// get the web token
+const jwt = require('jsonwebtoken');
+
+// get token for authorization header, take token and run it through method in JWT
+module.exports = (req, res, next) => {
+  try {
+    const token = req.header.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, 'secret');
+    // send back into user data, token is going back into the session
+    req.userData = decoded;
+    next();
+  } catch(error) {
+      return res.status(401).json({
+        message: 'Authorization failed'
+      });
+  }
+};
