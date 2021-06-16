1c76b326ebffcdad03f540ea2dc8b925eb842029
alekay <alekaymusic@gmail.com>
2021-06-16 16:53:30 -0400

    feat: add admin sign up

--- book-app-backend/api/routes/user.js
+++ book-app-backend/api/routes/user.js
@@ -12,7 +12,7 @@ const bcrypt = require('bcrypt');
 // get package for using JWTs "jot"
 const jwt = require('jsonwebtoken');
 
-// route for sign uo
+// route for sign-uo
 router.post('/signup', (req, res, next) => {
   // checking if the email already exists
   console.log('req.body.email')
@@ -23,16 +23,13 @@ router.post('/signup', (req, res, next) => {
       return res.status(409).json({
         message: 'Email already exists'
       });
-    }
-    // if error
-    else { 
+    } else { /* if there is an error*/
       bcrypt.hash(req.body.password, 10, (err, hash) => {
         if (err) {
           return res.status(500).json ({
             error: err
           })
-        } 
-        else {
+        }  else {
           // creating an instance of User Model
           const user = new User({
             // we will let mongo generate the id, json object
@@ -45,17 +42,59 @@ router.post('/signup', (req, res, next) => {
           user.save()
           .then(result => {
             res.status(201).json({
-              message: 'User Created Successfully'
+              message: 'User created successfully'
             });
           })
           .catch(err => {
             res.status(500).json({
-              message: 'Failure To Create User',
+              message: 'Failure to create user',
               error: err
             });
           });
         } 
-     })
+      })
     }
   })
 })
+
+// add admin sign-up, uses 
+router.post('/admin/signup', (req, res, next => {
+  console.log('req.body.email')
+  User.find({ email: req.body.email })
+  .exec().then(user => {
+    if (user.lenght >=1) {
+      return res.status(409).json({
+        message: 'Email already exists'
+      });
+    } 
+    else {
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if(err) {
+          return res.status(500).json({
+            error: err
+          })
+        } else {
+          const user = new User ({
+            _id: new mongoose.Types.ObjectId(),
+            error: req.body.email,
+            password: hash,
+            name: req.body.name,
+            user_type: admin
+          })
+          user.save
+          .then(result => {
+            res.status(201).json({
+              message: 'Admin successfuly created'
+            });
+          })
+          .catch(err => {
+            res.status(500).json({
+              message: 'Failure to create admin',
+              error: err
+            });
+          });
+        }
+      })
+    }
+  })
+}))
