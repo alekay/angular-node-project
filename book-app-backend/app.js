276e19505916adec48269f794117573c34740dc3
alekay <alekaymusic@gmail.com>
2021-06-16 19:38:55 -0400

    fix: update mongoDB connect

--- book-app-backend/app.js
+++ book-app-backend/app.js
@@ -8,12 +8,20 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv:alekay:mastadon@cluster0.kclkf.mongodb.net/ANGULAR-NODEJS-SERVER?retryWrites=true&w=majority');
+// import from MongoDB Atlas
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://alekay:<password>@cluster0.kclkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 
 // use dev token for morgan
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(bodyParser.json());
-
-module.exports = app;
