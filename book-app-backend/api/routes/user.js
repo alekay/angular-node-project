46b56d07edfced2f1a92360f68c50c5f036323e2
alekay <alekaymusic@gmail.com>
2021-06-16 23:25:04 -0400

    fix: adjust displaced formatting

--- book-app-backend/api/routes/user.js
+++ book-app-backend/api/routes/user.js
@@ -101,52 +101,53 @@ router.post('/admin-signup', (req, res, next) => {
 
 // user routing for log on
 router.post('/login', (req, res, next) => {
-
   console.log("BE-user.js - starting login()");
   console.log(req.body.email);
   console.log(req.body.password);
   User.find({email:req.body.email})
   .exec()
+  // we have a new user, do something
   .then(user => {
-      if(user.length < 1) {
-          return res.status(401).json({
-              message:'Auth failure'
-          });
+    if(user.length < 1) {
+      return(401).json({
+        message: 'Auth failure'
+      });
+    }
+    // use bcrypt to do password comparison, see if user can log on - hash password and then hash against that hash - will produce match if the password is there
+    bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+      if(err) {
+        return res.status(401).json({
+          message: 'Auth failed'
+        });
+      } 
+      // log user in with JWT
+      if(result) {
+        const token = jwt.sign({
+          email: user[0],
+          userID: user[0]._id
+        },
+        'secret', {
+          expiresIn:"1h"
+        }
+        )
+        return res.status(200).json({
+          message: 'Auth successful', 
+          user_type: user[0].user_type,
+          token: token
+        });
       }
-      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-          if(err) {
-              return res.status(401).json({
-                  message:'Auth failed'
-              });
-          }
-          if(result) {
-              const token = jwt.sign({
-                  email:user[0].email,
-                  userId: user[0]._id
-              },
-              'secret',
-              {
-                  expiresIn:"1h"
-              }
-              )
-              return res.status(200).json({
-                  message:"Auth successful",
-                  user_type:user[0].user_type,
-                  token:token
-              });
-          }
-          res.status(401).json({
-              mesasge:"Auth Failure"
-          });
-      })
+      res.status(401).json({
+        mesasge: "Auth Failure"
+      });
+    })
   })
   .catch(err => {
-      console.log("BE-user.js - error in login()");
+    console.log("BE-user.js - error in login()");
       res.status(500).json({
-          error:err
+        error: err
       });
+    });
   });
-});
 
 // export the module
 module.exports = router;
